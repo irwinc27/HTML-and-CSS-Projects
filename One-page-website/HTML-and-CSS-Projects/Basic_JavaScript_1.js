f2a422754741ab1e6bbbf05ff80b778649063328
irwinc27 <irwinc27@gmail.com>
2024-09-02 10:52:33 +0100

    Add files via upload

---
+++ One-page-website/HTML-and-CSS-Projects/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+
+alert("Hello World!");
+	function My_First_Function()  {
+		var String = "Kiss me im Irish/";
+		var Result = String,fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result; 
+	}
